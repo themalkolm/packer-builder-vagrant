8487a8115c5c89656c5fb0450dfb98e403eff494
Alexander Krasnukhin <the.malkolm@gmail.com>
2016-12-06 23:03:00 +0100

    Moar logs

--- builder.go
+++ builder.go
@@ -27,6 +27,7 @@ func (b *Builder) Prepare(raws ...interface{}) ([]string, error) {
 }
 
 func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packer.Artifact, error) {
+	ui.Message("Builder source_path ...")
 	c := b.config
 	if _, ok := c.BuilderConfig["source_path"]; !ok {
 		sourcePath, err := fetchBoxFile(c.URL, c.Name, c.Version, c.Provider, c.BoxFile)
@@ -35,23 +36,42 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		}
 		c.BuilderConfig["source_path"] = sourcePath
 	}
+	ui.Message(fmt.Sprintf("Builder source_path: %s", c.BuilderConfig["source_path"]))
 
+	ui.Message("Builder type ...")
 	builderType, err := c.builderType()
 	if err != nil {
 		return nil, err
 	}
+	ui.Message(fmt.Sprintf("Builder type: %s", builderType))
 
+	ui.Message("Builder ...")
 	builder, found := command.Builders[builderType];
 	if !found {
 		return nil, fmt.Errorf("unsupported builder type: %s", builderType)
 	}
+	ui.Message("Builder: OK")
 
+	ui.Message("Builder prepare ...")
 	b.builder = builder
-	_, err = b.builder.Prepare(c.BuilderConfig)
+	warnings, err := b.builder.Prepare(c.BuilderConfig)
 	if err != nil {
 		return nil, err
 	}
-	return b.builder.Run(ui, hook, cache)
+	if warnings != nil && len(warnings) > 0 {
+		for _, w := range warnings {
+			ui.Message(fmt.Sprintf("WARNING: %s", w))
+		}
+	}
+	ui.Message("Builder prepare: OK")
+
+	ui.Message("Builder run ...")
+	a, err := b.builder.Run(ui, hook, cache)
+	if err != nil {
+		return nil, err
+	}
+	ui.Message("Builder run: OK")
+	return a, nil
 }
 
 func (b *Builder) Cancel() {
